38fa634804bbe49b75ecfa197100031811d1bc97
Auric <112778782+AuricOls@users.noreply.github.com>
2024-06-05 20:17:55 -0600

    Complete exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+// crear un nuevo array donde se almacenaran los elementos que no se van a eliminar (clean)
+
+const removeFromArray = function(array, ...args) {
+    if(args.length == 0){
+        args =[];
+    }
+    let cleanArray = []
+
+    for(let i=0;i<array.length;i++){
+        
+        if(!args.includes(array[i])){
+            cleanArray.push(array[i]);
+        }
+    }
+    return cleanArray;
 };
 
 // Do not edit below this line
