e99ec7b62eed08d818bbb92f87220330acd210eb
Auric <112778782+AuricOls@users.noreply.github.com>
2024-06-05 20:55:31 -0600

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return Math.round(((degreesF - 32)*(5/9)) * 10)/10;
 };
-
-const convertToFahrenheit = function() {
+//Math.round(number * 10) / 10
+const convertToFahrenheit = function(degreesC) {
+  return Math.round(((degreesC * 1.8) + 32) * 10)/10
 };
 
 // Do not edit below this line
