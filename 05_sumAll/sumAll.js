631ab7caba943fff427e67410f50728669f5b54e
Auric <112778782+AuricOls@users.noreply.github.com>
2024-06-05 20:34:55 -0600

    Complete exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+
+
+const sumAll = function(limit1, limit2) {
+
+    if (typeof limit1 !== 'number' || typeof limit2!== 'number' || limit1 < 0 || limit2 < 0) {
+        return "ERROR";
+    }
+
+    let lower = Math.min(limit1, limit2);
+    let upper = Math.max(limit1, limit2);
+    let sum = 0;
+    for(let i = lower;i<=upper;i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
