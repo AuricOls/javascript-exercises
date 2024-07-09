714d28e14aec88a1085bd31a06fc6da031fd1558
Auric <112778782+AuricOls@users.noreply.github.com>
2024-07-09 16:40:25 -0600

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let curr = 1;
+    let prev = 0;
+    let temp = 0;
+    if (n == 0){
+        return 0;
+    }
+    if(n<0){
+        return "OOPS"
+    }
+    for (let i=1; i<n;i++){
+        temp = curr + prev;
+        prev = curr;
+        curr = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
