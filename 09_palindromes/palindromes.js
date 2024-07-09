45e553222d007fb0ee0286078e6940592c0dcd97
Auric <112778782+AuricOls@users.noreply.github.com>
2024-07-09 16:17:13 -0600

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let cleanWord = (word.replace(/[^a-zA-Z0-9]/g, "")).toLowerCase();
+    let arr = cleanWord.split("");
+    let rra = arr.slice()
+    rra.reverse();
+    console.log(arr);
+    console.log("----------------");
+    console.log(rra);
+    return arr.every((char, index) => char === rra[index]);
 };
 
+console.log(palindromes("Racecar!"));
 // Do not edit below this line
+
 module.exports = palindromes;
